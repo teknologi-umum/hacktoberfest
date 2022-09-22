f6cdef431ff9fed83730f0a074ecfcea9706fe1d
Ii64人 <richiisei@gmail.com>
2022-09-22 15:19:38 +0700

    feat(backend): CLI arg support, put env var as fallback (#5)

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,5 +1,7 @@
+use clap::{clap_app, value_t};
 use actix_web::{App, get, HttpResponse, HttpServer, web, Result};
-use std::{io, env};
+use core::num;
+use std::{io, env, usize};
 use std::collections::HashMap;
 use std::process::exit;
 
@@ -13,28 +15,56 @@ async fn healthcheck() -> Result<HttpResponse> {
     Ok(HttpResponse::Ok().json(resp))
 }
 
-#[actix_web::main]
-async fn main() -> io::Result<()> {
-    let listen_address = env::var("LISTEN_ADDRESS").unwrap_or(String::from("127.0.0.1:8080"));
-    let num_workers_str = env::var("NUM_WORKERS").unwrap_or(String::from("1"));
-    let mut num_workers: usize;
-
-    match num_workers_str.parse::<usize>() {
-                Err(e) => {
-                    println!("NUM_WORKERS is not a number!")
-                    exit(1);
-                }
-        Ok(n) => {
-            num_workers = n;
-        }
-
+#[tokio::main]
+async fn main() {    
+    if let Err(e) = run().await {
+        eprintln!("fatal {}", e);
+        std::process::exit(1);
     }
+}
+
+struct RunContext {
+    listen_address: String,
+    num_workers: usize,
+}
+
+async fn run() -> Result<()> {
+    let app = clap_app!(hacktoberfestd => 
+        (version: "")
+        (about: "Hacktoberfest serverd")
+        (@arg addr: --addr +takes_value "Listen address for HTTP server")
+        (@arg wrk: --wrk +takes_value "Number of HTTP server workers")
+    ).get_matches();
+    
+    let falback_laddr = env::var("LISTEN_ADDR").unwrap_or("127.0.0.1:8080".into());
+    let fallback_num_wrk_str = env::var("NUM_WORKERS").unwrap_or("1".into());
+    let mut fallback_num_wrk = 1;
+    if let Ok(num_wrk) = fallback_num_wrk_str.parse::<usize>() {
+        fallback_num_wrk = num_wrk;
+    }
+
+    let laddr = app.value_of("addr").unwrap_or(&falback_laddr[..]);
+    let num_wrk = value_t!(app, "wrk", usize).unwrap_or(fallback_num_wrk);
+    
+    let env = RunContext {
+        listen_address: String::from(laddr),
+        num_workers: num_wrk,
+    };
+
+    if let Err(e) = run_server(env).await {
+        return Err(e.into())
+    }
+    Ok(())
+}
+
+async fn run_server(env: RunContext) -> Result<(), std::io::Error> {
+    println!("run server {}", env.listen_address);
     HttpServer::new(|| {
         App::new()
             .service(healthcheck)
     })
-        .bind(listen_address)?
-        .workers(num_workers)
+        .bind(env.listen_address)?
+        .workers(env.num_workers)
         .run()
         .await
 }
