f596447bfabcdaea626f1c92f737504d007f1431
Nugraha <richiisei@gmail.com>
2022-09-24 15:10:14 +0700

    feat(backend): generic response wrapper, impl get rate_limit

--- backend/src/github.rs
+++ backend/src/github.rs
@@ -5,14 +5,11 @@ use reqwest::{
     header::{HeaderMap, HeaderValue},
     Client, Response, StatusCode,
 };
-use serde::{Deserialize, Serialize};
+use serde::{Deserialize, Serialize, de::DeserializeOwned};
 
-#[derive(Serialize, Deserialize)]
-pub struct Asd {
-    #[serde(rename = "resources")]
+#[derive(Serialize, Deserialize, Debug)]
+pub struct RateLimitResponse {
     resources: Resources,
-
-    #[serde(rename = "rate")]
     rate: Rate,
 }
 #[derive(Serialize, Deserialize, Debug)]
@@ -57,7 +54,7 @@ impl Rate {
     }
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Resources {
     pub core: Rate,
     pub graphql: Rate,
@@ -178,11 +175,38 @@ impl Github {
         Github { client }
     }
 
+    async fn response_wrapper<T>(response: Response) -> Result<T, GithubError>
+    where
+        T: DeserializeOwned + 'static,
+    {
+        match response.status() {
+            StatusCode::OK => {
+                let json_response = response.json::<T>().await
+                    .map_err(GithubError::Requwuest);
+                return json_response;
+            },
+            StatusCode::FORBIDDEN => {
+                return Err(GithubError::App(GithubErrorMetadata::from_http_respsonse(response).await));
+            }
+            status_code => {
+                return Err(GithubError::StatusCode(status_code))
+            }
+        }
+    }
+
+    /// rate_limit 
+    /// 
+    /// rate limit info
+    pub async fn rate_limit(&self) -> Result<RateLimitResponse, GithubError> {
+        let response = self.client
+            .get("https://api.github.com/rate_limit")
+            .send().await.map_err(GithubError::Requwuest)?;
+        Self::response_wrapper(response).await
+    }
+
     /// list_repostory
     /// 
     pub async fn list_repository(&self) -> Result<Vec<Repository>, GithubError> {
-        
-
         let response: Response = self.client
             .get("https://api.github.com/users/teknologi-umum/repos")
             .query(&[
@@ -192,19 +216,7 @@ impl Github {
             ])
             .send()
             .await.map_err(GithubError::Requwuest)?;
-        match response.status() {
-            StatusCode::OK => {
-                let json_response = response.json::<Vec<Repository>>().await
-                    .map_err(GithubError::Requwuest);
-                return json_response
-            },
-            StatusCode::FORBIDDEN => {
-                return Err(GithubError::App(GithubErrorMetadata::from_http_respsonse(response).await))
-            },
-            status_code => {
-                return Err(GithubError::StatusCode(status_code))
-            },
-        }
+        Self::response_wrapper(response).await
     }
 
     /// list_issues
@@ -217,23 +229,16 @@ impl Github {
             .get(u)
             .send()
             .await.map_err(GithubError::Requwuest)?;
-        match response.status() {
-            StatusCode::OK => {
-                let json_response = response.json::<Vec<Issue>>().await
-                    .map_err(GithubError::Requwuest)?;
-                let clean_issues = json_response
-                    .into_iter()
-                    .filter(|iss| !iss.node_id.starts_with("PR_"))
-                    .collect();
-                return Ok(clean_issues);
-            },
-            StatusCode::FORBIDDEN => { // For Biden
-                return Err(GithubError::App(GithubErrorMetadata::from_http_respsonse(response).await));
-            }
-            status_code => {
-                return Err(GithubError::StatusCode(status_code));
-            }
+        let resp = Self::response_wrapper::<Vec<Issue>>(response).await;
+        if let Ok(json_response) = resp {
+            let clean_issues = json_response
+                .into_iter()
+                .filter(|issue| !issue.node_id.starts_with("PR_"))
+                .collect();
+            return Ok(clean_issues);
         }
+
+        resp
     }
 
     /// list_languages
@@ -246,36 +251,28 @@ impl Github {
             .get(u)
             .send()
             .await.map_err(GithubError::Requwuest)?;
-        match response.status() {
-            StatusCode::OK => {
-                let mut json_response = response.json::<HashMap<String, i64>>().await
-                    .map_err(GithubError::Requwuest)?;
-                let mut language_set = vec![];
-                
-                for (key, value) in &json_response {
-                    language_set.push((key.into(), *value));
-                }
-                json_response.clear();
+        let resp = Self::response_wrapper::<HashMap<String, i64>>(response).await;
+        if let Ok(mut json_response) = resp {
+            let mut language_set = vec![];
+            for (key, value) in &json_response {
+                language_set.push((key.into(), *value));
+            }
+            json_response.clear();
 
-                language_set.sort_by(|a, b| {
-                    let (_, a_bytes) = a;
-                    let (_, b_bytes) = b;
-                    b_bytes.cmp(a_bytes)
-                });
+            language_set.sort_by(|a, b| {
+                let (_, a_bytes) = a;
+                let (_, b_bytes) = b;
+                b_bytes.cmp(a_bytes)
+            });
 
-                let languages = language_set
-                    .into_iter()
-                    .map(|(l, _)| l)
-                    .collect();
-                return Ok(languages);
-            },
-            StatusCode::FORBIDDEN => {
-                return Err(GithubError::App(GithubErrorMetadata::from_http_respsonse(response).await));
-            },
-            status_code => {
-                return Err(GithubError::StatusCode(status_code));
-            },
+            let languages = language_set
+                .into_iter()
+                .map(|(l, _)| l)
+                .collect();
+            return Ok(languages);
         }
+        
+        Err(resp.err().unwrap())
     }
 }
 
@@ -310,6 +307,13 @@ mod tests {
             }
         }
         Ok(())
+    }
+
+    #[tokio::test] 
+    async fn test_rate_limit() {
+        let gh = Github::new();
+        let rate_limit = gh.rate_limit().await.unwrap();
+        println!("{:?}", rate_limit);
     }
 
     #[tokio::test]
