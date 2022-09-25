b217c08d69ff5334eb2a19393952b33b73939c0d
elianiva <dicha.arkana03@gmail.com>
2022-09-25 08:51:34 +0700

    feat(frontend): more icons

---
+++ frontend/src/components/icons/ic_kotlin.tsx
@@ -0,0 +1,7 @@
+export function KotlinIcon() {
+  return (
+    <svg width="1.75em" height="1.75em" viewBox="0 0 24 24">
+      <path fill="currentColor" d="M2 2h20L12 12l10 10H2Z"></path>
+    </svg>
+  );
+}
