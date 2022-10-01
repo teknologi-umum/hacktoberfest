16f5ebcca15d0fd2924328fee72949a2750fd4d3
elianiva <dicha.arkana03@gmail.com>
2022-10-01 02:01:58 -0700

    use better algorithm for extracting categories from repositories (#53)
    * style(frontend): format with prettier
    
    * refactor(frontend): use better algorithm to get categories

--- frontend/src/services/list-categories.ts
+++ frontend/src/services/list-categories.ts
@@ -1,15 +1,17 @@
 import type { Repository } from "~/models/repository";
 
 export function getCategoriesList(repositories: Repository[]): string[] {
-  return [
-    ...new Set(
-      repositories
-        .map((repository) =>
-          repository.issues
-            .map((issue) => issue.labels.map((label) => label.name))
-            .flat()
-        )
-        .flat()
-    ),
-  ];
+  const uniqueLabels = new Set<string>();
+
+  for (const repository of repositories) {
+    for (const issue of repository.issues) {
+      for (const label of issue.labels) {
+        // ignore hacktoberfest label since they all should have it
+        if (label.name === "hacktoberfest") continue;
+        uniqueLabels.add(label.name);
+      }
+    }
+  }
+
+  return [...uniqueLabels];
 }
