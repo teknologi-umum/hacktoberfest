dcb9ae425c5964d29b6021f4008498d3b9c8b81d
Krishna <42563517+krowter@users.noreply.github.com>
2023-10-12 12:35:13 +0700

    fix: exclude bots from contributors list (#88)

--- frontend/src/services/list-contributors.ts
+++ frontend/src/services/list-contributors.ts
@@ -2,8 +2,10 @@ import { $ } from "@builder.io/qwik";
 import { API_BASE_URL } from "~/env";
 import { Contributor } from "~/models/contributor";
 
-export const getContributorsList = $(async (): Promise<Contributor[]> => {
+export const getContributorsList = $(async () => {
   const url = new URL("/contrib", API_BASE_URL);
   const response = await fetch(url);
-  return response.json();
+  const contributors: Contributor[] = await response.json();
+
+  return contributors.filter(({ full_name }) => !full_name.includes("[bot]"));
 });
